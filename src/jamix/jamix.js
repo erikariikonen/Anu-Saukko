fad895e2cce3545a756d3e04a9c27283994414ca
erikariikonen <katarina.riikonen@gmail.com>
2023-12-02 00:11:48 +0200

    jamix fixes

--- src/jamix/jamix.js
+++ src/jamix/jamix.js
@@ -1,53 +1,52 @@
 const axios = require('axios');
 
 const getTodaysFood = async (restaurant) => {
-  try {
-    let apiUrl;
+    try {
+        let apiUrl;
     
-    // API urlit
-    if (restaurant.toLowerCase() === 'signe') {
-      apiUrl = 'http://fi.jamix.cloud/apps/menuservice/rest/haku/menu/97325/7?lang=fi';
-    } else if (restaurant.toLowerCase() === 'ellen') {
-      apiUrl = 'http://fi.jamix.cloud/apps/menuservice/rest/haku/menu/97325/6?lang=fi';
-    } else {
-      throw new Error('Invalid restaurant. Please specify "signe" or "ellen".');
-    }
+        // API urlit
+        if (restaurant.toLowerCase() === 'signe') {
+            apiUrl = 'http://fi.jamix.cloud/apps/menuservice/rest/haku/menu/97325/7?lang=fi';
+        } else if (restaurant.toLowerCase() === 'ellen') {
+            apiUrl = 'http://fi.jamix.cloud/apps/menuservice/rest/haku/menu/97325/6?lang=fi';
+        } else {
+            throw new Error('Invalid restaurant. Please specify "signe" or "ellen".');
+        }
 
-    const response = await axios.get(apiUrl);
+        const response = await axios.get(apiUrl);
 
-    const currentDayOfWeek = new Date().getDay();
+        const currentDayOfWeek = new Date().getDay();
 
-    const todayMenu = response.data[0].menuTypes[0].menus.find(menu => {
-      const availableWeekdays = menu.days.map(day => day.weekday);
+        const todayMenu = response.data[0].menuTypes[0].menus.find(menu => {
+            const availableWeekdays = menu.days.map(day => day.weekday);
 
-      return availableWeekdays.includes(currentDayOfWeek);
-    });
+            return availableWeekdays.includes(currentDayOfWeek);
+        });
 
-    if (!todayMenu) {
-      return '';
-    }
+        if (!todayMenu) {
+            return '';
+        }
 
-    const mealOptions = todayMenu.days.find(day => day.weekday === currentDayOfWeek).mealoptions;
+        const mealOptions = todayMenu.days.find(day => day.weekday === currentDayOfWeek).mealoptions;
 
-    if (!mealOptions || mealOptions.length === 0) {
-      return '';
-    }
+        if (!mealOptions || mealOptions.length === 0) {
+            return '';
+        }
 
-    const todayFood = mealOptions.map(option => {
-      const menuItemsWithDiets = option.menuItems.map(item => {
-        const diets = item.diets ? ` (${item.diets.replace(/\s/g, '')})` : '';
-        const itemWithDiets = `**${item.name}**${diets}`;
-        return itemWithDiets;
-      });
-      return menuItemsWithDiets.join('\n');
-    }).join('\n\n');
+        const todayFood = mealOptions.map(option => {
+            const menuItemsWithDiets = option.menuItems.map(item => {
+                const diets = item.diets ? ` (${item.diets.replace(/\s/g, '')})` : '';
+                const itemWithDiets = `**${item.name}**${diets}`;
+                return itemWithDiets;
+            });
+            return menuItemsWithDiets.join('\n');
+        }).join('\n\n');
     
-    return todayFood;
-  } catch (error) {
-    console.error('Error fetching data from Jamix API:', error);
-    // Don't throw an error here, just return an empty string
-    return '';
-  }
+        return todayFood;
+    } catch (error) {
+        console.error('Error fetching data from Jamix API:', error);
+        return '';
+    }
 };
 
 module.exports = { getTodaysFood };
